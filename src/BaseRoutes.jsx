a7ed4e80f9ce13e43582d529d18c856dc8430088
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-06-04 15:27:46 -0400

    Move install path to a config file.

--- src/BaseRoutes.jsx
+++ src/BaseRoutes.jsx
@@ -6,6 +6,7 @@ import {
   useLocation,
   useNavigate,
 } from 'react-router-dom'
+import {INSTALL_PATH} from './Config'
 import ShareRoutes from './ShareRoutes'
 import debug from './utils/debug'
 
@@ -14,12 +15,12 @@ import debug from './utils/debug'
  * From URL design: https://github.com/bldrs-ai/Share/wiki/URL-Structure
  * ... We adopt a URL structure similar to Google Apps URL structure:
  *
- *   http://host/<app>/<view>/<object>
+ *   http://host/<install_path>/<app>/<view>/<object>
  *
  * which when fully expanded becomes:
  *
- *   http://host/share/v/p/indec.ifc
- *   http://host/share/v/gh/bldrs-ai/Share/main/public/index.ifc
+ *   http://host/<install_path>/share/v/p/indec.ifc
+ *   http://host/<install_path>/share/v/gh/bldrs-ai/Share/main/public/index.ifc
  *
  * @param {testElt} For unit test allow use of a stub here instead of loading the app.
  * @return {Object}
@@ -54,6 +55,3 @@ export default function BaseRoutes({testElt = null}) {
     </Routes>
   )
 }
-
-
-export const INSTALL_PATH = '/Share'
---
+++ src/Config.js
@@ -0,0 +1,7 @@
+/**
+ * The installation path of the app.  To serve on GitHub Pages, this
+ * should be the name of your repo.  If you cloned the project, just
+ * leave this as share.  If you renamed the repo, change this to the
+ * name of your new repo.
+ */
+export const INSTALL_PATH = '/Share'
